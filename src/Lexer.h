7f4817d7d2b9974edabd28689d9ca0f9dfdfb583
Vetrox <39677514+Vetrox@users.noreply.github.com>
2022-09-14 11:17:09 +0200

    basic lexer data members

--- src/Lexer.h
+++ src/Lexer.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <iostream>
+
+class AbstractLanguageItem {
+    virtual std::string to_string() = 0;
+};
+
+class Expression : AbstractLanguageItem {
+    virtual std::string to_string() override = 0;
+};
+
+class Lambda : Expression {
+    virtual std::string to_string() override;
+};
+
+class List : Expression {
+    virtual std::string to_string() override;
+};
+
+class PreConditions : List { // list-of-function (inputs -> bool)
+
+};
+
+class PostConditions : List { // list-of-function (outputs -> bool)
+
+};
